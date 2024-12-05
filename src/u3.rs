467749607ce4196c1d08cf8fb83af70609a63fe4
davidon-top <me@davidon.top>
2024-12-05 13:29:09 +0100

    u3

--- src/u3.rs
+++ src/u3.rs
@@ -1,1 +1,20 @@
-pub fn main() {}
+pub fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let a: u128 = input.trim().parse().unwrap();
+    input.clear();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let b: u128 = input.trim().parse().unwrap();
+    input.clear();
+    
+    devidable(a, b);
+}
+
+fn devidable(a: u128, b: u128) {
+    for i in a..b+1 {
+        let digit_sum: u128 = i.to_string().chars().map(|c| c.to_digit(10).unwrap() as u128).sum();
+        if digit_sum % 3 == 0 {
+            println!("{}", i);
+        }
+    }
+}
