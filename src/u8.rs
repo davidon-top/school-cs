b8cfc80d1b0a76ec5dd94f51a417d6bbc265269f
davidon-top <me@davidon.top>
2024-12-06 09:11:10 +0100

    u8

--- src/u8.rs
+++ src/u8.rs
@@ -1,1 +1,24 @@
-pub fn main() {}
+pub fn main() {
+    let mut a1 = 0;
+    let mut a2 = 0;
+    let mut a3 = 0;
+    let mut a4 = 0;
+    let mut a5 = 0;
+    
+    for _ in 0..30 {
+        let randnum = rand::random::<f32>();
+        let randnum = randnum * 5.0;
+        let randnum = randnum as i32;
+        
+        match randnum {
+            0 => a1 += 1,
+            1 => a2 += 1,
+            2 => a3 += 1,
+            3 => a4 += 1,
+            4 => a5 += 1,
+            _ => panic!("Invalid number"),
+        }
+    }
+    
+    println!("a1: {}, a2: {}, a3: {}, a4: {}, a5: {}", a1, a2, a3, a4, a5);
+}
