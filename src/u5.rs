65411da66cc71386763067f964423e8218df7d50
davidon-top <me@davidon.top>
2024-12-06 08:39:40 +0100

    u5

--- src/u5.rs
+++ src/u5.rs
@@ -1,1 +1,27 @@
-pub fn main() {}
+use std::io::Write;
+use std::ops::{Add, Sub};
+
+pub fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let a: i128 = input.trim().parse().unwrap();
+    input.clear();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let b: i128 = input.trim().parse().unwrap();
+    input.clear();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let op = input.trim().clone();
+
+    let fun = match op {
+        "+" | "sucet" | "add" | "sum" => i128::add,
+        "-" | "rozdiel" | "difference" | "subtract" => i128::sub,
+        "priemer" | "avg" | "average" => |a, b| (a + b) / 2,
+        _ => panic!("Nepodporovana operacia"),
+    };
+
+    let out = fun(a, b);
+    println!("{}", out);
+    
+    let mut file = std::fs::OpenOptions::new().write(true).create(true).open("output.txt").unwrap();
+    file.write_all(out.to_string().as_bytes()).unwrap();
+}
