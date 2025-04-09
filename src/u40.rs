680dd2dc941c16c4c1cc4d9172da9395affec3c4
davidon-top <me@davidon.top>
2025-04-09 08:48:16 +0200

    39-42

--- src/u40.rs
+++ src/u40.rs
@@ -1,1 +1,7 @@
-pub fn main() {}
+pub fn main() {
+	let mut sutaziaci = vec![("aoeu", 21), ("htns", 25), ("pyfg", 16)];
+	sutaziaci.sort_by(|s, l| {s.1.cmp(&l.1)});
+	sutaziaci.iter().for_each(|n| {
+		println!("{} {}", n.0, n.1);
+	})
+}
