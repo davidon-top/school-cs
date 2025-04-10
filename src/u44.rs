ae70fd2920a95ff74448ff1409ab129462af7eca
davidontop <me@davidon.top>
2025-04-10 14:50:25 +0200

    44

--- src/u44.rs
+++ src/u44.rs
@@ -1,1 +1,12 @@
-pub fn main() {}
+pub fn main() {
+    println!("hmotnost: (v kg)");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let hmotnost: f32 = input.trim().parse().unwrap();
+    input.clear();
+    println!("viska (v m)");
+    std::io::stdin().read_line(&mut input).unwrap();
+    let viska: f32 = input.trim().parse().unwrap();
+    let bmi = hmotnost / viska.powi(2);
+    println!("bmi: {bmi}")
+}
